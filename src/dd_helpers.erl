4c694aa7b48224846c55d0161fead13223709b63
Gert Meulyzer <cel357@gmail.com>
2014-09-04 10:58:46 +0200

    Added relaxed parameter to the httpc:request calls

--- src/dd_helpers.erl
+++ src/dd_helpers.erl
@@ -48,11 +48,11 @@ get_urls(Bin) ->
 
 -spec http_get(string()) -> any().
 http_get(Url) ->
-    httpc:request(get, {Url, [{"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0"}]}, [{autoredirect, true}], []).
+    httpc:request(get, {Url, [{"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0"}]}, [{autoredirect, true}, {relaxed, true}], []).
 
 -spec http_head(string()) -> any().
 http_head(Url) ->
-    httpc:request(head, {Url, [{"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0"}]}, [{autoredirect, true}], []).
+    httpc:request(head, {Url, [{"User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0"}]}, [{autoredirect, true}, {relaxed, true}], []).
 
 -spec module_exists(module()) -> boolean().
 module_exists(Module) ->
